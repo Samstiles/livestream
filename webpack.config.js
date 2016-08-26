07364fb281e4c9a859912f51512e63a83b6a18ab
Sam Stiles <samuel.dev.stiles@gmail.com>
2016-08-26 20:25:59 -0300

    use webpack file loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,25 @@ module.exports = {
   eslint: '.eslintrc',
   entry: './src/index.js',
   output: {
-    path: __dirname + '/compiled',
+    path: __dirname + '/build',
     filename: 'index.bundle.js'
   },
   module: {
     preLoaders: [
       {
         test: /\.js$/,
-        loader: "eslint-loader",
+        loader: 'eslint-loader',
         exclude: /(node_modules|compiled)/
       }
     ],
     loaders: [
       {
+        test: /\.jpe?g$|\.gif$|\.png$|\.svg$/,
+        loader: 'file'
+      },
+      {
         test: /\.less$/,
-        loader: "style!css!less"
+        loader: 'style!css!less'
       },
       {
         test: /\.js$/,
